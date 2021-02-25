7c5ecc5e3eb36fbb37c777a01fb9c976f2102bcc
francescapetra <francescalaurapetra@gmail.com>
2021-02-25 14:46:05 +0100

    generatore-psw

---
+++ script.js
@@ -0,0 +1,4 @@
+var nome = prompt("Qual è' il tuo nome'?");
+var cognome = prompt("Qual è il tuo cognome?");
+var colore = prompt("Quale è il tuo colore preferito?");
+document.getElementById('new_psw').innerHTML = nome + cognome + colore + "27";
